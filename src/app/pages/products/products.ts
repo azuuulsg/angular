2a0b391ea215a1a55b5f412036b5104dbf31b468
azuuulsg <azulsgl@gmail.com>
2025-10-09 16:51:49 -0300

    repaso de interfaz y directiva ngfor; aplicamos las unidades de tamaño desde el html; aplicamos modal para ver mas informacion de tarjetas

--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -14,47 +14,53 @@ export class Products {
   propiedad privada: contenido restringido
   
   */
-    public infoTarjeta: Tarjeta[];
+  public infoTarjeta: Tarjeta[];
 
-    constructor(){
-      this.infoTarjeta =[
-        {
-          id: "1T",
-          img: "../../../assets/ramo-de-12-tulipanes.jpg",
-          alt: "tulipanes",
-          titulo: "flores",
-          descripcion: "variedad de flores",
-          anualPublicacion: 2025
-        },
+  constructor() {
+    this.infoTarjeta = [
+      {
+        id: "1T",
+        img: "../../../assets/flores.jpg",
+        alt: "tulipanes",
+        titulo: "flores",
+        descripcion: "variedad de flores",
+        anualPublicacion: 2025
+      },
 
-        {
-          id: "2T",
-          img: "../../../assets/dalia_01-flickr-Waldemar_Jan-768x480.jpg",
-          alt: "flores",
-          titulo: "rosas",
-          descripcion: "rosas y blancas",
-          anualPublicacion: 2024
-        },
+      {
+        id: "2T",
+        img: "../../../assets/dalia_01-flickr-Waldemar_Jan-768x480.jpg",
+        alt: "flores",
+        titulo: "rosas",
+        descripcion: "rosas y blancas",
+        anualPublicacion: 2024
+      },
 
-        {
-          id: "3T",
-          img: "../../../assets/Peonias.webp",
-          alt: "flores",
-          titulo: "peonias",
-          descripcion: "Las peonías son nativas de Asia",
-          anualPublicacion: 2024
-        },
+      {
+        id: "3T",
+        img: "../../../assets/Peonias.webp",
+        alt: "flores",
+        titulo: "peonias",
+        descripcion: "son nativas de Asia",
+        anualPublicacion: 2024
+      },
 
-        {
-          id: "4T",
-          img:"../../../assets/ramo-de-12-tulipanes.jpg",
-          alt: "flores",
-          titulo: "tulipanes",
-          descripcion: "las Tulines es un género de plantas perennes",
-          anualPublicacion: 2025
-        }
-      ]
+      {
+        id: "4T",
+        img: "../../../assets/ramo-de-12-tulipanes.jpg",
+        alt: "flores",
+        titulo: "tulipanes",
+        descripcion: " género de plantas perenes",
+        anualPublicacion: 2025
+      }
+    ]
 
-      
+    
+  }
+
+  tarjetaSeleccionada: any = null;
+    verMasinfo(tarjeta: any){
+      this.tarjetaSeleccionada = tarjeta;
     }
+
 }
